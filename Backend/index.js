9e213a67bb315aef680d6118660cedd26d124a38
chandra <chandrashekharaprasad@gmail.com>
2025-10-25 10:45:27 +0530

    adding login and sign up backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,22 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
-//config env
 dotenv.config();
+connectDB();
 
-const app =express();
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-app.get('/',(req,res)=>{
-    res.send("Running Backend");
-})
+app.get("/", (req, res) => {
+  res.send("Running Backend");
+});
 
-app.listen(port,()=>{
-    console.log(`App running on ${port}`)
-})
+app.use("/api/users", userRoutes);
 
+app.listen(port, () => console.log(`App running on port ${port}`));
