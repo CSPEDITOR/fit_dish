9e2d1ab9f57178efd38973b89281a6c11ff63a11
Aditya <adityaanupam96@gmail.com>
2025-10-30 17:15:31 +0530

    Design Changes

--- frontend/src/pages/Home/HomeHeader.jsx
+++ frontend/src/pages/Home/HomeHeader.jsx
@@ -6,15 +6,16 @@ function HomeHeader() {
     <div className='home-header  mt-8'>
       <div className='flex items-center justify-center'>
         <div className="slogan w-full">
-          <h1 className='text-7xl text-nowrap t-2 w-fit'>Healthy <span className='t-red'>Eating is</span></h1>
-          <h1 className='text-7xl text-nowrap t-2 w-fit'><span className='t-red'>an</span> <span className='t-orange'>Important </span>Part</h1>
-          <h1 className='text-7xl text-nowrap t-2 w-fit'>of Lifestyle </h1>
+          <h1 className='text-8xl text-nowrap t-2 w-fit'>Healthy <span className='t-red'>Eating is</span></h1>
+          <h1 className='text-8xl text-nowrap t-2 w-fit'><span className='t-red'>an</span> <span className='t-orange'>Important </span>Part</h1>
+          <h1 className='text-8xl text-nowrap t-2 w-fit'>of Lifestyle </h1>
+          <h3 className='t-2 t-gray text-2xl my-8'>We prepare delicious food for <br /> you we are always</h3>
+          <button className='bg-red rounded-full text-white px-4 py-2 '>Explore Now</button>
         </div>
         <div className='w-[40%] h-fit scale-150'>
           <img src={dishImg} alt="" className='w-full aspect-square' />
         </div>
       </div>
-      <h3 className='t-2'>We prepare delicious food for <br /> you we are always</h3>
     </div>
   )
 }
